802d113242d500bfc827f0fe16e52f1f42447039
benjamin.brajon@etu.univ-lyon1.fr <benjamin.brajon@etu.univ-lyon1.fr>
2024-12-05 11:14:36 +0100

    registration works

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -170,7 +170,6 @@ export function RegisterRoutes(app: Router) {
         });
         // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
         app.post('/users',
-            authenticateMiddleware([{"jwt":[]}]),
             ...(fetchMiddlewares<RequestHandler>(UserController)),
             ...(fetchMiddlewares<RequestHandler>(UserController.prototype.createUser)),
 
